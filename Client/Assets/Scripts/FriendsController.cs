ac41085295e24cd0b3bb6e8a4d727d5393bd942d
ProCH2405 <tranducchuan2006@gmail.com>
2026-04-11 19:31:01 +0700

    Hoan thien mot phan menu

---
+++ Client/Assets/Scripts/FriendsController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI; // Cần cái này để điều khiển Image
+
+public class FriendsController : MonoBehaviour
+{
+    public GameObject friendsPopup;
+
+    [Header("Tab Settings")]
+    public Image backgroundDisplay; // Kéo cái Background_Display vào đây
+    public Sprite spriteBanBe;      // Kéo ảnh "Tab Bạn bè đang sáng" vào đây
+    public Sprite spriteThemBan;    // Kéo ảnh "Tab Thêm bạn đang sáng" vào đây
+
+    public void ToggleFriendsPopup()
+    {
+        friendsPopup.SetActive(!friendsPopup.activeSelf);
+        // Mỗi lần mở lên thì mặc định hiện tab Bạn Bè trước
+        if (friendsPopup.activeSelf) ShowFriendsTab();
+    }
+
+    // Hàm gọi khi nhấn nút Tab_BanBe
+    public void ShowFriendsTab()
+    {
+        backgroundDisplay.sprite = spriteBanBe;
+        Debug.Log("Đã đổi sang ảnh Bạn bè");
+    }
+
+    // Hàm gọi khi nhấn nút Tab_ThemBan
+    public void ShowAddTab()
+    {
+        backgroundDisplay.sprite = spriteThemBan;
+        Debug.Log("Đã đổi sang ảnh Thêm bạn");
+    }
+}
---
+++ Client/Assets/Scripts/RankController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class RankController : MonoBehaviour
+{
+    public GameObject rankPopup;
+
+    [Header("Tab Sprites")]
+    public Image backgroundDisplay;
+    public Sprite spriteTop100;
+    public Sprite spriteFriends;
+
+    // 1. Hàm Toggle (Dùng cho nút Cúp vàng ở menu dưới)
+    public void ToggleRankPopup()
+    {
+        rankPopup.SetActive(!rankPopup.activeSelf);
+        if (rankPopup.activeSelf) ShowTop100();
+    }
+
+    // 2. Hàm Đóng chuyên biệt (Dùng cho nút X)
+    public void CloseRankPopup()
+    {
+        rankPopup.SetActive(false);
+    }
+
+    public void ShowTop100()
+    {
+        backgroundDisplay.sprite = spriteTop100;
+    }
+
+    public void ShowFriendsRank()
+    {
+        backgroundDisplay.sprite = spriteFriends;
+    }
+}
