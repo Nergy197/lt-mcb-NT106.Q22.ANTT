1e892a6c656f22ea76725c9b9402f633fcd2e109
Nergy197 <nguyen.manh.cuong.190706@gmail.com>
2026-04-12 15:44:37 +0700

    clean code battle sửa lại

--- Client/Assets/Scripts/Battle/Legacy/PokemonDataBox.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using UnityEngine.UI; // Cần dùng UI Image
-
-public class PokemonDataBox : MonoBehaviour
-{
-    // Chúng ta sẽ viết code hiệu ứng thanh máu rút từ từ ở đây sau.
-    // Tạm thời để trống để gắn component đã.
-}
