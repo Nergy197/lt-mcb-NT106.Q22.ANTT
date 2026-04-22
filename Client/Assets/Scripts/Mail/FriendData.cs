ceb06854a9dab644c76cca911fa64155672b1695
ProCH2405 <tranducchuan2006@gmail.com>
2026-04-22 13:34:30 +0700

    Add and update mail feature friends list

---
+++ Client/Assets/Scripts/Mail/FriendData.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+[Serializable] // Dòng này cực kỳ quan trọng để Unity đọc được dữ liệu
+public class FriendData
+{
+    public string playerId;
+    public string playerName;
+    public bool isOnline;
+    public string lastSeenAt;
+}
